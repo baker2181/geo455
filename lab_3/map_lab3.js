39b3d38cc47137b3fd160f49ae24be57c86a92da
baker2181 <98429313+baker2181@users.noreply.github.com>
2022-02-14 16:36:56 -0600

    Add files via upload

--- lab_3/map_lab3.js
+++ lab_3/map_lab3.js
@@ -1,5 +1,4 @@
 var mymap = L.map("map").setView([38.576654997239075, -92.1756964845525], 8);
-
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYWJha2VzOSIsImEiOiJja3o3ajBnMXAwanhiMm9tcDVoOGptNTl3In0.efDBfxtTqOfoOTw5Oo5Sgg', {
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
