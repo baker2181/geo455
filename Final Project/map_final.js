cb7ec26f91857e2d957ebf7d5cd67427fb3b6c48
baker2181 <98429313+baker2181@users.noreply.github.com>
2022-04-26 17:31:41 -0500

    Add files via upload

---
+++ Final Project/map_final.js
@@ -0,0 +1,205 @@
+// Create custom popups with images
+var angelsPopup = "Angels Stadium of Anaheim<br/>Team: Los Angeles Angels<br/>Opening Year: 1966<br/>Capacity: 45,517<br/><img src='https://upload.wikimedia.org/wikipedia/commons/4/4a/Angel%27s_Stadium_2009.jpg' width='150px'/>";
+
+var dbacksPopup = "Chase Field<br/>Team: Arizona Diamondbacks<br/>Opening Year: 1998<br/>Capacity: 48,686<br/><img src='https://upload.wikimedia.org/wikipedia/commons/7/78/Chase_Field_-_2011-07-11_-_Interior_North_Upper.jpg' width='150px'/>";
+
+var bravesPopup = "Truist Park<br/>Team: Atlanta Braves<br/>Opening Year: 2017<br/>Capacity: 41,084<br/><img src='https://upload.wikimedia.org/wikipedia/commons/5/52/SunTrust_Park_viewed_from_upper_decks_behind_home_plate%2C_May_2017.jpg', width='150px'/>";
+
+var oriolesPopup = "Oriole Park at Camden Yards<br/>Team: Baltimore Orioles<br/>Opening Year: 1992<br/>Capacity: 45,971<br/><img src='https://upload.wikimedia.org/wikipedia/commons/c/c3/OrioleParkAtCamdenYardsJune2013.jpg', width='150px'/>";
+
+var redsoxPopup = "Fenway Park<br/>Team: Boston Red Sox<br/>Opening Year: 1912<br/>Capacity: 37,755<br/><img src='https://live.staticflickr.com/3704/9068339276_ebc235c949_b.jpg', width='150px'/>";
+
+var cubsPopup = "Wrigley Field<br/>Team: Chicago Cubs<br/>Opening Year: 1914<br/>Capacity: 41,649<br/><img src='https://upload.wikimedia.org/wikipedia/commons/c/c0/Wrigley_Field_%2815993624310%29.jpg', width='150px'/>";
+
+var whitesoxPopup = "Guaranteed Rate Field<br/>Team: Chicago White Sox<br/>Opening Year: 1991<br/>Capacity: 40,615<br/><img src='https://upload.wikimedia.org/wikipedia/commons/e/e2/U.S._Cellular_Field_%2830972191694%29.jpg', width='150px'/>";
+
+var redsPopup = "Great American Ball Park<br/>Team: Cincinnati Reds<br/>Opening Year: 2003<br/>Capacity: 42,319<br/><img src='https://upload.wikimedia.org/wikipedia/commons/6/61/Great_American_Ball_Park_%2833464240496%29.jpg', width='150px'/>";
+
+var guardiansPopup = "Progressive Field<br/>Team: Cleveland Guardians<br/>Opening Year: 1994<br/>Capacity: 35,000<br/><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Progressive_Field%2C_hours_before_Game_1_of_the_2016_World_Series._%2830447296182%29.jpg/800px-Progressive_Field%2C_hours_before_Game_1_of_the_2016_World_Series._%2830447296182%29.jpg?20161105092805', width='150px'/>";
+
+var rockiesPopup = "Coors Field<br/>Team: Colorado Rockies<br/>Opening Year: 1995<br/>Capacity: 50,445<br/><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Coors_field_1.JPG/800px-Coors_field_1.JPG?20160727193747', width='150px'/>";
+
+var tigersPopup = "Comerica Park<br/>Team: Detroit Tigers<br/>Opening Year: 2000<br/>Capacity: 41,083<br/><img src='https://upload.wikimedia.org/wikipedia/commons/1/19/Comerica-Park-Detroit-MI-Panorama.jpg', width='150px'/>";
+
+var marlinsPopup = "Marlins Park<br/>Team: Miami Marlins<br/>Opening Year: 2012<br/>Capacity: 36,742<br/><img src='https://upload.wikimedia.org/wikipedia/commons/f/fa/Marlins_First_Pitch_at_Marlins_Park%2C_April_4%2C_2012_%28cropped%29.jpg', width='150px'/>";
+
+var astrosPopup = "Minute Maid Park<br/>Team: Houston Astros<br/>Opening Year: 2000<br/>Capacity: 41,168<br/><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/Minute_Maid_Park_2010.JPG/799px-Minute_Maid_Park_2010.JPG?20110521133757', width='150px'/>";
+
+var royalsPopup = "Kauffman Stadium<br/>Team: Kansas City Royals<br/>Opening Year: 1973<br/>Capacity: 37,903<br/><img src='https://upload.wikimedia.org/wikipedia/commons/d/da/Kauffman_Stadium_at_night%2C_2009.jpg', width='150px'/>";
+
+var dodgersPopup = "Dodger Stadium<br/>Team: Los Angeles Dodgers<br/>Opening Year: 1962<br/>Capacity: 56,000<br/><img src='https://upload.wikimedia.org/wikipedia/commons/f/f1/Dodger_Stadium.jpg', width='150px'/>";
+
+var brewersPopup = "American Family Field<br/>Team: Milwaukee Brewers<br/>Opening Year: 2001<br/>Capacity: 41,900<br/><img src='https://upload.wikimedia.org/wikipedia/commons/6/6b/Miller_Park0001.jpg', width='150px'/>";
+
+var twinsPopup = "Target Field<br/>Team: Minnesota Twins<br/>Opening Year: 2010<br/>Capacity: 38,544<br/><img src='https://upload.wikimedia.org/wikipedia/commons/b/b7/Target_Field_2016.jpg', width='150px'/>";
+
+var nationalsPopup = "Nationals Park<br/>Team: Washington Nationals<br/>Opening Year: 2008<br/>Capacity: 41,339<br/><img src='https://upload.wikimedia.org/wikipedia/commons/5/55/Opening_of_Nationals_Park_-_039_%282377924697%29.jpg', width='150px'/>";
+
+var metsPopup = "Citi Field<br/>Team: New York Mets<br/>Opening Year: 2009<br/>Capacity: 41,922<br/><img src='https://upload.wikimedia.org/wikipedia/commons/8/8d/Citi_Field_2011.JPG', width='150px'/>";
+
+var yankeesPopup = "Yankee Stadium<br/>Team: New York Yankees<br/>Opening Year: 2009<br/>Capacity: 47,309<br/><img src='https://upload.wikimedia.org/wikipedia/commons/4/44/Yankee_Stadium_upper_deck_2010.jpg', width='150px'/>";
+
+var asPopup = "OAC Coliseum<br/>Team: Oakland Athletics<br/>Opening Year: 1966<br/>Capacity: 46,847<br/><img src='https://upload.wikimedia.org/wikipedia/commons/8/89/McAfee_Coliseum_%2815993646150%29.jpg', width='150px'/>";
+
+var philliesPopup = "Citizens Bank Park<br/>Team: Philadelphia Phillies<br/>Opening Year: 2004<br/>Capacity: 42,792<br/><img src='https://upload.wikimedia.org/wikipedia/commons/f/f6/Citizens_Bank_Park_2021.jpg', width='150px'/>";
+
+var piratesPopup = "PNC Park<br/>Team: Pittsburgh Pirates<br/>Opening Year: 2001<br/>Capacity: 38,747<br/><img src='https://upload.wikimedia.org/wikipedia/commons/1/1a/PNC_Park%2C_Home_of_Pittsburgh_Pirates.jpg', width='150px'/>";
+
+var padresPopup = "PETCO Park<br/>Team: San Diego Padres<br/>Opening Year: 2004<br/>Capacity: 40,209<br/><img src='https://upload.wikimedia.org/wikipedia/commons/0/0b/Petco_Park_%2815994877519%29.jpg', width='150px'/>";
+
+var giantsPopup = "AT&T Park<br/>Team: San Fransico Giants<br/>Opening Year: 2000<br/>Capacity: 41,265<br/><img src='https://upload.wikimedia.org/wikipedia/commons/7/7c/AT%26T_Park_Overlooking_San_Francisco_Bay.jpg', width='150px'/>";
+
+var marinersPopup = "Safeco Field<br/>Team: Seattle Mariners<br/>Opening Year: 1999<br/>Capacity: 47,929<br/><img src='https://upload.wikimedia.org/wikipedia/commons/6/65/Safeco_field_from_upper_deck.jpg', width='150px'/>";
+
+var cardinalsPopup = "Busch Stadium<br/>Team: St. Louis Cardinals<br/>Opening Year: 2006<br/>Capacity: 45,494<br/><img src='https://upload.wikimedia.org/wikipedia/commons/f/f4/Busch_Stadium_III_%2816180972535%29.jpg', width='150px'/>";
+
+var raysPopup = "Tropicana Field<br/>Team: Tampa Bay Rays<br/>Opening Year: 1990<br/>Capacity: 25,000<br/><img src='https://upload.wikimedia.org/wikipedia/commons/e/e1/Tropicana_Field_Playing_Field_Opening_Day_2010.JPG', width='150px'/>";
+
+var rangersPopup = "Globe Life Park in Arlington<br/>Team: Texas Rangers<br/>Opening Year: 2020<br/>Capacity: 40,300<br/><img src='https://upload.wikimedia.org/wikipedia/commons/f/fc/Globelifefield_june2020.jpg', width='150px'/>";
+
+var bluejaysPopup = "Rogers Centre<br/>Team: Toronto Blue Jays<br/>Opening Year: 1989<br/>Capacity: 49,282<br/><img src='https://upload.wikimedia.org/wikipedia/commons/c/cb/Skydome_Rogers_Center_Toronto_Canada.jpg', width='150px'/>";
+
+var customOptions ={'maxWidth': '150','className' : 'custom'};
+
+// Set up landmark variables and individual landmarks
+coords = [
+    [41.948438, -87.655333],
+    [39.284052, -76.621512],
+    [42.346676, -71.097218],
+    [41.496211, -81.685229],
+    [43.641438, -79.389353],
+    [33.800308, -117.882732],
+    [40.829643, -73.926175],
+    [42.346676, -71.097218],
+    [43.641438, -79.389353],
+    [37.752483, -122.19821],
+    [34.073851, -118.239958],
+    [44.981712, -93.27776],
+    [43.641438, -79.389353],
+    [27.768225, -82.653392],
+    [40.757088, -73.845821],
+    [42.346676, -71.097218]
+    
+];
+
+var stadiums = L.layerGroup();
+
+var angels = L.marker([33.800308, -117.882732]).bindPopup(angelsPopup, customOptions).addTo(stadiums);
+var dbacks = L.marker([33.445526, -112.066664]).bindPopup(dbacksPopup, customOptions).addTo(stadiums);
+var braves = L.marker([33.8907, -84.467684]).bindPopup(bravesPopup, customOptions).addTo(stadiums);
+var orioles = L.marker([39.284052, -76.621512]).bindPopup(oriolesPopup, customOptions).addTo(stadiums);
+var redsox = L.marker([42.346676, -71.097218]).bindPopup(redsoxPopup, customOptions).addTo(stadiums);
+var cubs = L.marker([41.948438, -87.655333]).bindPopup(cubsPopup, customOptions).addTo(stadiums);
+var whitesox = L.marker([41.8299023, -87.633752]).bindPopup(whitesoxPopup, customOptions).addTo(stadiums);
+var reds = L.marker([39.097931, -84.508151]).bindPopup(redsPopup, customOptions).addTo(stadiums);
+var guardians = L.marker([41.496211, -81.685229]).bindPopup(guardiansPopup, customOptions).addTo(stadiums);
+var rockies = L.marker([39.755882, -104.994178]).bindPopup(rockiesPopup, customOptions).addTo(stadiums);
+var tigers = L.marker([42.338998, -83.04852]).bindPopup(tigersPopup, customOptions).addTo(stadiums);
+var marlins = L.marker([25.778318, -80.219597]).bindPopup(marlinsPopup, customOptions).addTo(stadiums);
+var astros = L.marker([29.757697, -95.354538]).bindPopup(astrosPopup, customOptions).addTo(stadiums);
+var royals = L.marker([39.051672, -94.480314]).bindPopup(royalsPopup, customOptions).addTo(stadiums);
+var dodgers = L.marker([34.073851, -118.239958]).bindPopup(dodgersPopup, customOptions).addTo(stadiums);
+var brewers = L.marker([43.027978, -87.97115]).bindPopup(brewersPopup, customOptions).addTo(stadiums);
+var twins = L.marker([44.981712, -93.27776]).bindPopup(twinsPopup, customOptions).addTo(stadiums);
+var nationals = L.marker([38.87301, -77.007433]).bindPopup(nationalsPopup, customOptions).addTo(stadiums);
+var mets = L.marker([40.757088, -73.845821]).bindPopup(metsPopup, customOptions).addTo(stadiums);
+var yankees = L.marker([40.829643, -73.926175]).bindPopup(yankeesPopup, customOptions).addTo(stadiums);
+var as = L.marker([37.752483, -122.19821]).bindPopup(asPopup, customOptions).addTo(stadiums);
+var phillies = L.marker([39.906057, -75.166495]).bindPopup(philliesPopup, customOptions).addTo(stadiums);
+var pirates = L.marker([40.446855, -80.005666]).bindPopup(piratesPopup, customOptions).addTo(stadiums);
+var padres = L.marker([32.707656, -117.156904]).bindPopup(padresPopup, customOptions).addTo(stadiums);
+var giants = L.marker([37.778595, -122.38927]).bindPopup(giantsPopup, customOptions).addTo(stadiums);
+var mariners = L.marker([47.591391, -122.332327]).bindPopup(marinersPopup, customOptions).addTo(stadiums);
+var cardinals = L.marker([38.622619, -90.192821]).bindPopup(cardinalsPopup, customOptions).addTo(stadiums);
+var rays = L.marker([27.768225, -82.653392]).bindPopup(raysPopup, customOptions).addTo(stadiums);
+var rangers = L.marker([32.75128, -97.082504]).bindPopup(rangersPopup, customOptions).addTo(stadiums);
+var bluejays = L.marker([43.641438, -79.389353]).bindPopup(bluejaysPopup, customOptions).addTo(stadiums);
+
+var al = L.layerGroup();
+var angels = L.marker([33.800308, -117.882732]).bindPopup(angelsPopup, customOptions).addTo(al);
+var orioles = L.marker([39.284052, -76.621512]).bindPopup(oriolesPopup, customOptions).addTo(al);
+var redsox = L.marker([42.346676, -71.097218]).bindPopup(redsoxPopup, customOptions).addTo(al);
+var whitesox = L.marker([41.8299023, -87.633752]).bindPopup(whitesoxPopup, customOptions).addTo(al);
+var guardians = L.marker([41.496211, -81.685229]).bindPopup(guardiansPopup, customOptions).addTo(al);
+var tigers = L.marker([42.338998, -83.04852]).bindPopup(tigersPopup, customOptions).addTo(al);
+var astros = L.marker([29.757697, -95.354538]).bindPopup(astrosPopup, customOptions).addTo(al);
+var royals = L.marker([39.051672, -94.480314]).bindPopup(royalsPopup, customOptions).addTo(al);
+var twins = L.marker([44.981712, -93.27776]).bindPopup(twinsPopup, customOptions).addTo(al);
+var yankees = L.marker([40.829643, -73.926175]).bindPopup(yankeesPopup, customOptions).addTo(al);
+var as = L.marker([37.752483, -122.19821]).bindPopup(asPopup, customOptions).addTo(al);
+var mariners = L.marker([47.591391, -122.332327]).bindPopup(marinersPopup, customOptions).addTo(al);
+var rays = L.marker([27.768225, -82.653392]).bindPopup(raysPopup, customOptions).addTo(al);
+var rangers = L.marker([32.75128, -97.082504]).bindPopup(rangersPopup, customOptions).addTo(al);
+var bluejays = L.marker([43.641438, -79.389353]).bindPopup(bluejaysPopup, customOptions).addTo(al);
+
+var nl = L.layerGroup();
+var dbacks = L.marker([33.445526, -112.066664]).bindPopup(dbacksPopup, customOptions).addTo(nl);
+var braves = L.marker([33.8907, -84.467684]).bindPopup(bravesPopup, customOptions).addTo(nl);
+var cubs = L.marker([41.948438, -87.655333]).bindPopup(cubsPopup, customOptions).addTo(nl);
+var reds = L.marker([39.097931, -84.508151]).bindPopup(redsPopup, customOptions).addTo(nl);
+var rockies = L.marker([39.755882, -104.994178]).bindPopup(rockiesPopup, customOptions).addTo(nl);
+var marlins = L.marker([25.778318, -80.219597]).bindPopup(marlinsPopup, customOptions).addTo(nl);
+var dodgers = L.marker([34.073851, -118.239958]).bindPopup(dodgersPopup, customOptions).addTo(nl);
+var brewers = L.marker([43.027978, -87.97115]).bindPopup(brewersPopup, customOptions).addTo(nl);
+var nationals = L.marker([38.87301, -77.007433]).bindPopup(nationalsPopup, customOptions).addTo(nl);
+var mets = L.marker([40.757088, -73.845821]).bindPopup(metsPopup, customOptions).addTo(nl);
+var phillies = L.marker([39.906057, -75.166495]).bindPopup(philliesPopup, customOptions).addTo(nl);
+var pirates = L.marker([40.446855, -80.005666]).bindPopup(piratesPopup, customOptions).addTo(nl);
+var padres = L.marker([32.707656, -117.156904]).bindPopup(padresPopup, customOptions).addTo(nl);
+var giants = L.marker([37.778595, -122.38927]).bindPopup(giantsPopup, customOptions).addTo(nl);
+var cardinals = L.marker([38.622619, -90.192821]).bindPopup(cardinalsPopup, customOptions).addTo(nl);
+
+
+//Set up the baselayers and WMS layer
+var streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ2NoYXVkaHVyaSIsImEiOiJjazBtcG5odG8wMDltM2JtcjdnYTgyanBnIn0.qwqjMomdrBMG36GQKXBlMw', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1
+});
+
+var grayscale = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ2NoYXVkaHVyaSIsImEiOiJjazBtcG5odG8wMDltM2JtcjdnYTgyanBnIn0.qwqjMomdrBMG36GQKXBlMw', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox/light-v9',
+    tileSize: 512,
+    zoomOffset: -1
+});
+
+var topo = L.tileLayer.wms('http://ows.mundialis.de/services/service?', {
+    layers: 'SRTM30-Colored-Hillshade'
+});
+
+
+//Create the map variable
+var mymap = L.map("map", {
+    center: [40, -95], 
+    zoom: 5,
+    layers: [streets, stadiums]});
+
+var line = L.polyline(coords, {color: "black", weight: 4});
+line.addTo(mymap);
+
+// Create menu items
+var baseLayers = {
+    'Streets': streets,
+    'Grayscale': grayscale,
+    };
+
+var overlays = {'Stadiums': stadiums, 'American League': al, 'National League': nl, 'Rich Hill Career Path': line};
+
+//Create the menu
+ var layerControl = L.control.layers(baseLayers, overlays, {collapsed: false}).addTo(mymap); //collapsed: true is defaults
+
+L.easyButton(('<img src="globe_icon.png", height=85%>'), function(btn, map){
+    map.setView([40, -95], 5);
+}).addTo(mymap);
+
+var miniMap = new L.Control.MiniMap(L.tileLayer('https://api.maptiler.com/maps/topo/{z}/{x}/{y}.png?key=tZnptaeI9RvKHsX18rbW'), {
+    toggleDisplay: true,
+    minimized: true,
+    position: 'bottomright'
+}).addTo(mymap);
